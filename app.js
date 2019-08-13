3a7f76c5bbd17f9e0e8b124197bbdc873c8f4d7c
Yuka Takahashi <yukatkh@gmail.com>
2019-08-12 23:03:25 -0700

    call ./test, output and input

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 const electron = require("electron");
 const { spawn } = require("child_process");
-const ls = spawn('/home/yuka/Halide/apps/autoscheduler/a.out',
+const ls = spawn('/home/yuka/Halide/apps/autoscheduler/test',
   {
     env: {
       'LD_LIBRARY_PATH':'/home/yuka/Halide/apps/autoscheduler/bin/',
@@ -10,21 +10,26 @@ const ls = spawn('/home/yuka/Halide/apps/autoscheduler/a.out',
     },
   });
 
+const fs = require('fs');
+fs.readFile("/home/yuka/Halide/apps/autoscheduler/test.cpp", 'utf-8', (err, data) => {
+        document.getElementById("algorithm").innerText += data;
+    });
+
 ls.on('error', console.log);
 
 ls.stdout.on('data', (data) => {
-  const e = document.getElementById("stdout");
+  const e = document.getElementById("schedule");
   e.value += data;
   e.scrollTop = e.scrollHeight;
 });
 
 ls.stderr.on('data', (data) => {
-  const e = document.getElementById("stdout");
+  const e = document.getElementById("schedule");
   e.value += data;
   e.scrollTop = e.scrollHeight;
 });
 
-document.getElementById("stdin").addEventListener('keypress', (e) => {
+document.getElementById("input").addEventListener('keypress', (e) => {
   const key = e.keyCode;
   if (key !== 13) return;
 
