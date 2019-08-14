ec108220d2fef52641d2e31bdf72eb4e54888a34
Yuka Takahashi <yukatkh@gmail.com>
2019-08-13 21:48:00 -0700

    change indent

--- app.js
+++ app.js
@@ -3,38 +3,38 @@
 const electron = require("electron");
 const { spawn } = require("child_process");
 const ls = spawn('/home/yuka/Halide/apps/autoscheduler/test',
-  {
-    env: {
-      'LD_LIBRARY_PATH':'/home/yuka/Halide/apps/autoscheduler/bin/',
-      'HL_CYOS':'1',
-    },
-  });
+    {
+        env: {
+            'LD_LIBRARY_PATH':'/home/yuka/Halide/apps/autoscheduler/bin/',
+            'HL_CYOS':'1',
+        },
+    });
 
 const fs = require('fs');
 fs.readFile("/home/yuka/Halide/apps/autoscheduler/test.cpp", 'utf-8', (err, data) => {
-        document.getElementById("algorithm").value += data;
-    });
+    document.getElementById("algorithm").value += data;
+});
 
 ls.on('error', console.log);
 
 ls.stdout.on('data', (data) => {
-  const e = document.getElementById("schedule");
-  e.value = data;
-  e.scrollTop = e.scrollHeight;
+    const e = document.getElementById("schedule");
+    e.value = data;
+    e.scrollTop = e.scrollHeight;
 });
 
 ls.stderr.on('data', (data) => {
-  const e = document.getElementById("schedule");
-  e.value = data;
-  e.scrollTop = e.scrollHeight;
+    const e = document.getElementById("schedule");
+    e.value = data;
+    e.scrollTop = e.scrollHeight;
 });
 
 document.getElementById("input").addEventListener('keypress', (e) => {
-  const key = e.keyCode;
-  if (key !== 13) return;
+    const key = e.keyCode;
+    if (key !== 13) return;
 
-  const input = e.target.value;
-  e.target.value = '';
-  console.log(`write ${input}`);
-  ls.stdin.write(input + "\n");
+    const input = e.target.value;
+    e.target.value = '';
+    console.log(`write ${input}`);
+    ls.stdin.write(input + "\n");
 });
