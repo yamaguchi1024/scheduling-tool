ce8acf620e637f481b68e8c59de60f3641e1b88f
Yuka Takahashi <yukatkh@gmail.com>
2020-05-14 02:24:18 +0900

    Change var to let

--- js/svg.js
+++ js/svg.js
@@ -5,12 +5,12 @@ window.onload = function () {
 
 const fontsmall = "12px";
 const fontbig = "15px";
-// var image_width = 2400;
-// var image_height = 1600;
-// var segments = [[2, 146], [41, 11], [32, 1]];
-var image_width = 240;
-var image_height = 160;
-var segments = [
+// let image_width = 2400;
+// let image_height = 1600;
+// let segments = [[2, 146], [41, 11], [32, 1]];
+let image_width = 240;
+let image_height = 160;
+let segments = [
 	[-1, 1, 1, "source"],
 	[0, 4, 60, "parallel"],
 	[1, 21, 27, ""],
@@ -18,15 +18,15 @@ var segments = [
 	[1, 80, 27, ""],
 	[4, 8, 1, "vectorize"]];
 
-var segment_height = 25;
-var segment_x = 500; // 1150
+let segment_height = 25;
+let segment_x = 500; // 1150
 
-var p0;
-var p1;
+let p0;
+let p1;
 
-var outlets = [];
+let outlets = [];
 
-var snap;
+let snap;
 
 function draw() {
 
@@ -34,29 +34,23 @@ function draw() {
 	
 	snap.clear();
 
-	background_rect = snap.rect(0, 0, 500, 600).attr({ fill: "#ffffff", stroke: null, strokeWidth: 1 });
+	let widths = [image_width];
+	let heights = [image_height];
+	let y0 = 50;
 
-	var widths = [image_width];
-	var heights = [image_height];
+	for (let i = 0; i < segments.length; i++){
 
-	//var width = image_width;
-	//var height = image_height;
-	
-	var y0 = 50;
+		let parent = segments[i][0];
+		let x_range = segments[i][1];
+		let y_range = segments[i][2];
 
-	for (var i = 0; i < segments.length; i++){
+		let width = widths[parent+1];
+		let height = heights[parent+1];
 
-		var parent = segments[i][0];
-		var x_range = segments[i][1];
-		var y_range = segments[i][2];
+		let box_width = (1 + 0.5*Math.log(width)) * 140 / 2;
+		let box_height = (1 + 0.5* Math.log(height)) * 40 / 2;
 
-		var width = widths[parent+1];
-		var height = heights[parent+1];
-
-		var box_width = (1 + 0.5*Math.log(width)) * 140 / 2;
-		var box_height = (1 + 0.5* Math.log(height)) * 40 / 2;
-
-		var parent_outlet = null;
+		let parent_outlet = null;
 		if (parent != -1) 
 			parent_outlet = outlets[parent];
 		
@@ -76,11 +70,11 @@ function draw_segment(snap, x_offset, y_offset, width, height, x_range, y_range,
 
 	x_offset = x_offset - width;
 
-	var x0 = x_offset;
-	var x1 = x_offset+width;
-	var y0 = y_offset;
-	var y1 = y_offset + height;
-	var dx = height * 2;	
+	let x0 = x_offset;
+	let x1 = x_offset+width;
+	let y0 = y_offset;
+	let y1 = y_offset + height;
+	let dx = height * 2;	
 
 
 	draw_subbox(snap, x0, x1, y0, y1, dx, "#FFFFFF", "#000000")
@@ -90,13 +84,13 @@ function draw_segment(snap, x_offset, y_offset, width, height, x_range, y_range,
 		draw_arrow(parent_outlet[0], (y0 + y1) / 2, x0 - dx/2-30, (y0 + y1) / 2);
 	}
 
-	var next_data_width =  Math.ceil(data_width / x_range);
-	var next_data_height =  Math.ceil(data_height / y_range);
+	let next_data_width =  Math.ceil(data_width / x_range);
+	let next_data_height =  Math.ceil(data_height / y_range);
 
-	// var x_label = '' + data_width + '/' + x_range + "=" + next_data_width;
-	// var y_label = '' + data_height + '/' + y_range + "=" + next_data_height;
-	var x_label =  '0..' + (x_range-1);
-	var y_label = '0..' + (y_range-1);
+	// let x_label = '' + data_width + '/' + x_range + "=" + next_data_width;
+	// let y_label = '' + data_height + '/' + y_range + "=" + next_data_height;
+	let x_label =  '0..' + (x_range-1);
+	let y_label = '0..' + (y_range-1);
 
 	snap.text( (x0 + x1)/2-20, y0-6, x_label).attr({fontSize: fontsmall});
 	snap.text(x1-dx/2, y0 + height / 2 +20 , y_label).attr({fontSize: fontsmall});
@@ -108,15 +102,15 @@ function draw_segment(snap, x_offset, y_offset, width, height, x_range, y_range,
 
 	if (x_range > width/4)
 		x_range = width/4;
-	for (var y = 0; y<y_range; y++){
-		for (var x = 0; x < x_range; x++) {
-			var _x0 = x0 + width * x / x_range - dx * y / y_range;
-			var _x1 = x0 + width * (x+1) / x_range  - dx * y / y_range;
-			var _y0 = y0 + height * y / y_range;
-			var _y1 = y0 + height * (y+1) / y_range;
-			var _dx = dx / y_range;
-			var fill_color = "#FFFFFF"
-			var stroke_color = "#000000"
+	for (let y = 0; y<y_range; y++){
+		for (let x = 0; x < x_range; x++) {
+			let _x0 = x0 + width * x / x_range - dx * y / y_range;
+			let _x1 = x0 + width * (x+1) / x_range  - dx * y / y_range;
+			let _y0 = y0 + height * y / y_range;
+			let _y1 = y0 + height * (y+1) / y_range;
+			let _dx = dx / y_range;
+			let fill_color = "#FFFFFF"
+			let stroke_color = "#000000"
 			if (x == 0 && y == Math.ceil(y_range-1)) {
 				fill_color = "#FF8888";
 				stroke_color = "#FF0000";
@@ -134,7 +128,7 @@ function draw_segment(snap, x_offset, y_offset, width, height, x_range, y_range,
 		}
 	};
 	
-	var d = 2;
+	let d = 2;
 	snap.text( (x0 + x1)/2-dx/2-60+d, (y0+y1)/2+10+d, label).attr({fontWeight:"bold",fontSize:fontbig,fill:"#FFFFFF",});
 	snap.text( (x0 + x1)/2-dx/2-60-d, (y0+y1)/2+10+d, label).attr({fontWeight:"bold",fontSize:fontbig,fill:"#FFFFFF",});
 	snap.text( (x0 + x1)/2-dx/2-60-d, (y0+y1)/2+10-d, label).attr({fontWeight:"bold",fontSize:fontbig,fill:"#FFFFFF",});
@@ -153,7 +147,7 @@ function draw_segment(snap, x_offset, y_offset, width, height, x_range, y_range,
 	*/
 }
 function draw_subbox(snap, x0, x1, y0, y1, dx, fill_color, stroke_color) {
-	var points = [x0, y0, x1, y0, x1 - dx, y1, x0 - dx, y1];
+	let points = [x0, y0, x1, y0, x1 - dx, y1, x0 - dx, y1];
 	snap.polygon(points).attr({ fill: fill_color, stroke: stroke_color});
 	//snap.line(x0, y0, x1, y0).attr({ fill: "none", stroke: "#000000",  opacity: "0.5" });
 	//snap.line(x1, y0, x1-dx, y1).attr({ fill: "none", stroke: "#000000" ,  opacity: "0.5" });
@@ -164,7 +158,7 @@ function draw_subbox(snap, x0, x1, y0, y1, dx, fill_color, stroke_color) {
 function draw_arrow(x0, y0, x1, y1) {
 	snap.line(x0, y0, x1, y1).attr({ fill: "none", stroke: "#FF0000" });
 
-	var r = 10;
+	let r = 10;
 	snap.line(x1-r, y1-r, x1, y1).attr({ fill: "none", stroke: "#FF0000" });
 	snap.line(x1-r, y1+r, x1, y1).attr({ fill: "none", stroke: "#FF0000" });
 
