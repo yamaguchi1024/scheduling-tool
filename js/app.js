10ef382d63e406e5f3bd61f06a38a41b6d11c643
Yuka Takahashi <yukatkh@gmail.com>
2021-01-26 16:25:07 -0500

    Change visualization drastically

--- js/app.js
+++ js/app.js
@@ -11,7 +11,6 @@ const path = require('path');
 let filename = "/home/yuka/Halide/apps/scheduling-tool/test/unsharp_mask.cpp";
 let globalexec = execTest();
 let globalcolortable = {};
-let maxLineNum = 0;
 const colors = ["#884EA0", "#34495e", "#2471A3", "#172396", "#229954", "#65853c", "#96591b", "#a31e00"];
 
 ipcRenderer.on('fileopen', (event, str) => {
@@ -38,7 +37,6 @@ function execTest() {
     binary.on('error', console.log);
 
     binary.stdout.on('data', (data) => {
-        const e = document.getElementById("schedule");
         const iarray = data.toString().split(/\n/);
         let funcid;
         for (let element of iarray) {
@@ -101,15 +99,6 @@ function execTest() {
             }
             else if (json.type == "phase1")
             {
-                // Disable non-clickable scheduling buttons
-                const scheduleElements = document.getElementById("schedule").children;
-                for (let s of scheduleElements) {
-                    if (s.children == undefined) continue;
-                    const button = s.children[0];
-                    button.disabled = true;
-                }
-
-                maxLineNum = 0;
                 document.getElementById('tile').classList.remove('inactive');
                 const inst = document.getElementById("instruction");
                 inst.innerHTML = json.instruction;
@@ -118,7 +107,6 @@ function execTest() {
                 const suggest = document.getElementById("suggestion");
                 suggest.innerHTML = "";
                 const costarray = json.cost.split(/\n/);
-                const runtimearray = json.runtime.split(/\n/);
                 const loadcostarray = json.load_costs.split(/\n/);
                 const storecostarray = json.store_costs.split(/\n/);
                 const computecostarray = json.compute_costs.split(/\n/);
@@ -141,12 +129,8 @@ function execTest() {
                     button.style.backgroundColor = "#FFFFFF";
 
                     const cdiv = document.createElement("div");
-                    const rdiv = document.createElement("div");
                     cdiv.setAttribute("style", "text-align: right; float: right; height: 25px; margin-right: 6px;");
                     cdiv.innerHTML =  '<span style="color: black; font-family: monospace;">Cost: </span>' + costarray[i];
-                    rdiv.innerHTML =  parseFloat(runtimearray[i]).toFixed(3) + "ms";
-                    rdiv.setAttribute("style", "text-align: right; float: right; height: 25px;");
-                    rdiv.style.color = "#3189e8";
 
                     const c = document.createElement("div");
                     c.setAttribute("id", "popup_phase1");
@@ -168,7 +152,6 @@ function execTest() {
                     const div = document.createElement("div");
                     div.setAttribute("style", "padding: 0; margin: 0px;");
                     div.appendChild(button);
-                    //div.appendChild(rdiv);
                     div.appendChild(cdiv);
                     suggest.appendChild(div);
                 }
@@ -181,16 +164,6 @@ function execTest() {
                 e.innerHTML = json.contents;
                 document.getElementById("input").disabled = true;
                 funcid = node_attrs.find(v => v.label === json.func).id;
-
-                // Disable non-clickable scheduling buttons
-                const scheduleElements = document.getElementById("schedule").children;
-                for (let idx in scheduleElements) {
-                    if (scheduleElements[idx].children == undefined) continue;
-                    const button = scheduleElements[idx].children[0];
-                    if (idx > maxLineNum) {
-                        button.disabled = true;
-                    }
-                }
 
                 // Hide phase 1 items here
                 const suggest = document.getElementById("suggestion");
@@ -209,76 +182,35 @@ function execTest() {
             }
             else if (json.type == "line_cost")
             {
-                const e = document.getElementById("schedule");
                 const line_cost = json.costs;
                 const load_cost = json.load_costs;
                 const store_cost = json.store_costs;
                 const compute_cost = json.compute_costs;
-                const runtime = json.runtime;
                 const linenum = json.linenum;
-                const nodes = e.children;
-                for (let i in nodes) {
-                    if (i != linenum) continue;
-                    maxLineNum = i;
-                    const stage = nodes[i];
-                    if (stage.children == undefined) continue;
-                    const cost_div = stage.children[1];
-                    const cdiv = document.createElement("div");
-                    const rdiv = document.createElement("div");
-                    cdiv.setAttribute("style", "text-align: right; float: right; height: 25px; margin-right: 6px;");
-                    cdiv.style.color = "#FF6666";
-                    rdiv.setAttribute("style", "text-align: right; float: right; height: 25px;");
-                    rdiv.style.color = "#3189e8";
 
-                    cdiv.innerHTML =  '<span style="color: black; font-family: monospace;">Cost: </span>' + line_cost;
-                    rdiv.innerHTML = parseFloat(runtime).toFixed(3) + "ms";
+                draw_button_cost(linenum, line_cost, load_cost, store_cost, compute_cost);
 
-                    const c = document.createElement("div");
-                    c.setAttribute("style", "left: 10px; display: none; background-color: #DDDDDD; text-align: left; width: 200px; position: relative; z-index: 1; border: 1px solid black; color: black");
-                    c.innerHTML +=  "load cost: " + load_cost;
-                    c.innerHTML +=  "<br> store cost: " + store_cost;
-                    c.innerHTML +=  "<br> compute cost: " + compute_cost;
-
-                    cdiv.onmouseover = function() {
-                        cdiv.children[1].style.display = 'block';
-                    };
-                    cdiv.onmouseout = function() {
-                        cdiv.children[1].style.display = 'none';
-                    };
-
-                    cdiv.appendChild(c);
-                    //cost_div.appendChild(rdiv);
-                    cost_div.appendChild(cdiv);
-                }
             }else if (json.type == "schedule")
             {
-                const e = document.getElementById("schedule");
                 const lines = json.contents;
-                const phase = parseInt(json.phase);
-                const curfunc = json.func;
                 let functable = {};
                 const latestSeg = {};
                 latestSeg[0] = 0;
                 const parentSeg = {};
                 const segments = new Array(lines.length);
-                segments[0] = [-1, 1, 1, "source", "top"];
-                e.innerHTML = "";
+                segments[0] = [-1, 1, 1, "source", "top", ""];
                 for (const idx in lines) {
-                    let index = idx;
-                    let newline = "";
                     let func;
                     let vectorized = false;
                     let parallel = false;
+                    let sched_lines = [];
 
-                    const button = document.createElement("button");
-                    const hontai = document.createElement("div");
-                    let buttonbackgroundchange = true;
                     for (const iidx in lines[idx]) {
                         let curline = lines[idx][iidx];
 
                         if (curline.includes("for")) {
                             const nestcount = (curline.match(/&nbsp;/g) || []).length / 4;
-                            const l = curline.replace('&nbsp;', ' ');
+                            const l = curline.replace(/&nbsp;/g, " ");
                             let regexp = /for[ ]+(.+)\.(.)[ ]+in[ ]+0\.\.([0-9]+)/;
                             let m = l.match(regexp);
                             const fname = m[1];
@@ -289,8 +221,9 @@ function execTest() {
 
                             latestSeg[nestcount] = parseInt(idx);
                             parentSeg[idx] = latestSeg[nestcount - 1];
-                            if (xory == "x")
-                                segments[idx] = [parentSeg[idx], range, -1, "", fname];
+                            if (xory == "x") {
+                                segments[idx] = [parentSeg[idx], range, -1, "", fname, ""];
+                            }
                             if (xory == "y")
                                 segments[idx][2] = range;
                             if (vectorized)
@@ -301,22 +234,21 @@ function execTest() {
                         // vectorized?
                         if (curline.match(/(vectorized)/) != null)
                             vectorized = true;
-                        if (curline.match(/(parallel)/) != null)
+                        else if (curline.match(/(parallel)/) != null)
                             parallel = true;
+                        else
+                            sched_lines.push(curline);
 
                         let fname = curline.match(/[?].*[?]/);
                         if (fname != null) {
                             curline = curline.replace(fname,'');
                             func = fname[0].slice(1,-1);
                         }
-
-                        // parse line and identify this block
-                        if (iidx != lines[idx].length - 1) {
-                            curline += "<br>";
-                        }
-                        index += "<br>";
-                        newline += curline;
                     }
+
+                    if (segments[idx] != null)
+                        segments[idx][5] = sched_lines;
+
                     if (idx == 0) func = "top";
 
                     if (func in functable) functable[func].push(parseInt(idx));
@@ -324,45 +256,6 @@ function execTest() {
 
                     if (!(func in globalcolortable))
                         globalcolortable[func] = colors[Object.keys(globalcolortable).length%(colors.length)];
-
-                    let buttonbackground =
-                        buttonbackgroundchange ? globalcolortable[func] : "#E60000";
-
-                    button.innerHTML = index;
-                    hontai.innerHTML = newline;
-
-                    button.onclick = function() {
-                        globalexec.stdin.write(idx + " -1\n");
-                        document.getElementById("input").disabled = false;
-                    };
-                    let prevcolor;
-                    button.onmouseover = function() {
-                        prevcolor = button.style.backgroundColor;
-                        button.style.backgroundColor = "#FFEF00";
-
-                        updateHighlightFromSchedule(parseInt(idx));
-                    };
-                    button.onmouseout = function() {
-                        button.style.backgroundColor = prevcolor;
-
-                        restoreHighlightFromSchedule(parseInt(idx));
-                    };
-                    button.setAttribute("style", "color: #FFFFFF; text-align: left; float: left; font-family: monospace;");
-
-                    button.style.backgroundColor = buttonbackground;
-
-                    hontai.setAttribute("style", "text-align: left; font-family: monospace;");
-
-                    const linecost = document.createElement("div");
-                    linecost.setAttribute("style", "text-align: right; float: right;");
-                    linecost.setAttribute("id", "linecost");
-
-                    const div = document.createElement("div");
-                    div.setAttribute("style", "padding: 7px; margin: 0px; border-bottom: 1px dashed black;");
-                    div.appendChild(button);
-                    div.appendChild(hontai);
-                    div.appendChild(linecost);
-                    e.appendChild(div);
                 }
 
                 draw(segments);
@@ -397,15 +290,6 @@ function execTest() {
                 c.innerHTML += "<br>compute cost: " + compute_cost;
 
                 e.appendChild(c);
-            } else if (json.type == "realize" ) {
-                const e = document.getElementById("cost");
-
-                //e.appendChild(document.createTextNode(', Run Time: '));
-
-                //const span = document.createElement('span');
-                //span.setAttribute('style', "color: #3189e8;");
-                //span.innerText = `${json.contents.toFixed(3)}ms`;
-                //e.appendChild(span);
 
                 const undobutton = document.createElement("button");
                 undobutton.onclick = function() {
@@ -466,8 +350,8 @@ function execTest() {
     });
 
     binary.stderr.on('data', (data) => {
-        const e = document.getElementById("schedule");
-        e.innerHTML += data;
+        const e = document.getElementById("instruction");
+        e.innerHTML += " " + data;
         e.scrollTop = e.scrollHeight;
     });
 
--- js/svg.js
+++ js/svg.js
@@ -1,7 +1,3 @@
-window.onload = function () {
-    draw();
-}
-
 const fontsmall = "12px";
 const fontbig = "14px";
 let image_width = 2560;
@@ -9,14 +5,16 @@ let image_height = 1600;
 let segment_x = 510; // 1150
 
 let snap;
+let y_loc = [];
 
 function draw(segments) {
     snap = Snap("#svg");
     snap.clear();
+    y_loc = [];
 
     let widths = [image_width];
     let heights = [image_height];
-    let y0 = 50;
+    let y0 = 20;
     let outlets = [];
 
     for (let i = 0; i < segments.length; i++){
@@ -42,7 +40,9 @@ function draw(segments) {
         if (parent != -1) 
             parent_outlet = outlets[parent];
 
-        draw_segment(snap, segment_x, y0, box_width, box_height, x_range, y_range, width, height, parent_outlet, segments[i][3], segments[i][4], outlets, i);
+        draw_segment(snap, segment_x, y0, box_width, box_height,
+            x_range, y_range, width, height, parent_outlet,
+            segments[i][3], segments[i][4], outlets, i, segments[i][5]);
 
         y0 = y0 + box_height + 42;
 
@@ -52,7 +52,10 @@ function draw(segments) {
 
 
 }
-function draw_segment(snap, x_offset, y_offset, width, height, x_range, y_range, data_width, data_height, parent_outlet, label, funcname, outlets, segnum) {
+
+function draw_segment(snap, x_offset, y_offset, width, height,
+                x_range, y_range, data_width, data_height, parent_outlet,
+                                    label, funcname, outlets, segnum, schedule_line) {
     x_offset = x_offset - width;
 
     let x0 = x_offset;
@@ -61,7 +64,8 @@ function draw_segment(snap, x_offset, y_offset, width, height, x_range, y_range,
     let y1 = y_offset + height;
     let dx = height * 2;	
 
-    document.getElementById("svg").setAttribute("height", Math.max(parseInt(document.getElementById("svg").getAttribute("height")), y1 + 50));
+    document.getElementById("svg").setAttribute("height",
+        Math.max(parseInt(document.getElementById("svg").getAttribute("height")), y1 + 50));
     draw_subbox(snap, x0, x1, y0, y1, dx, "#FFFFFF", "#000000");
     if (parent_outlet != null) {
         const color = "#FF0000"; //globalcolortable[funcname];
@@ -114,11 +118,63 @@ function draw_segment(snap, x_offset, y_offset, width, height, x_range, y_range,
     snap.text( (x0 + x1)/2-dx/2-35-d, (y0+y1)/2+5-d, label).attr({fontWeight:"bold",fontSize:fontbig,fill:"#FFFFFF",});
     snap.text( (x0 + x1)/2-dx/2-35+d, (y0+y1)/2+5-d, label).attr({fontWeight:"bold",fontSize:fontbig,fill:"#FFFFFF",});
     snap.text( (x0 + x1)/2-dx/2-35, (y0+y1)/2+5, label).attr({fontWeight:"bold",fontSize:fontbig,fill:"#000000",});
-}
+
+    // Draw dash line for each schedule
+    const line_y = y1 + 17;
+    const line_x = x0 + 60;
+    snap.line(line_x, line_y, 850, line_y).attr({ fill: "none", stroke: "black", 'stroke-dasharray': "8"});
+
+    const sched_y = line_y - 32;
+    const sched_x = 600;
+    // Draw schedule text
+    for (const idx in schedule_line) {
+        let line = schedule_line[idx];
+        let fname = line.match(/[?].*[?]/);
+        if (fname != null) {
+            line = line.replace(fname,'');
+        }
+
+        const space = (line.match(/&nbsp;/g) || []).length;
+        snap.text(sched_x + space*6, sched_y + 14*idx, line.replace(/&nbsp;/g,"")).attr({fontSize: fontsmall});
+    }
+
+    y_loc.push(line_y);
+};
+
+function draw_button_cost(linenum, line_cost, load_cost, store_cost, compute_cost) {
+    if (snap == undefined) return;
+
+    // Draw scheduling button
+    const x = 860;
+    const y = y_loc[linenum];
+    let button = snap.polygon(x, y, x+10, y+10, x+10, y+3, x+20, y+3, x+20, y-3, x+10, y-3, x+10, y-10);
+    button     = button.attr({ text: linenum, id : "button", fill: "red"});
+
+    button.mouseover( function(e) {
+            e.target.setAttribute("fill", "#FFEF00");
+    });
+
+    button.click( function() {
+            globalexec.stdin.write(linenum + " -1\n");
+            document.getElementById("input").disabled = false;
+    });
+
+    button.mouseout( function(e) {
+            e.target.setAttribute("fill", "red");
+    });
+
+    // Draw cost
+    snap.text(x+30, y + 4, "Cost: " + line_cost).attr({fontSize: fontbig});
+    snap.text(x+100, y + 2, "Load cost: " + load_cost).attr({fontSize: fontsmall});
+    snap.text(x+100, y + 16, "Store cost: " + store_cost).attr({fontSize: fontsmall});
+    snap.text(x+100, y + 30, "Compute cost: " + compute_cost).attr({fontSize: fontsmall});
+};
+
 function draw_subbox(snap, x0, x1, y0, y1, dx, fill_color, stroke_color) {
     let points = [x0, y0, x1, y0, x1 - dx, y1, x0 - dx, y1];
     snap.polygon(points).attr({ fill: fill_color, stroke: stroke_color});
 }
+
 function draw_arrow(x0, y0, x1, y1, color) {
     let r = 10;
     snap.line(x0, y0, x1, y1).attr({ fill: "none", stroke: color });
